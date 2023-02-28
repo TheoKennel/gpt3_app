75d8dbde3b40f4c02ce205acc987402984d94e2a
TheoKenel <theokennel@gmail.com>
2023-02-28 11:45:11 +0100

    index.js rajout /client

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,11 +5,11 @@ import './whatGPT3.css';
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
-      <div className='gpt3__whatgps3-feature'>
+      <div className='gpt3__whatgpt3-feature'>
         <Feature />
       </div>
       <div className='gpt3__whatgpt3-heading'>
-        <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+        <h1 className='gradient__text'>The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className='gpt3__whatgpt3-container'>
@@ -18,7 +18,7 @@ const WhatGPT3 = () => {
         <Feature />
       </div>
     </div>
-  )
+  );
 }
 
 export default WhatGPT3
